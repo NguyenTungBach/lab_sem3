d56a6d6f11e829454dcd008ce326a9ceffacbfe1
NguyenTungBach <bachntth2010055@fpt.edu.vn>
2022-06-16 18:45:24 +0700

    bach

--- LabSem3/Migrations/Configuration.cs
+++ LabSem3/Migrations/Configuration.cs
@@ -1,6 +1,9 @@
 ﻿namespace LabSem3.Migrations
 {
+    using LabSem3.Enum;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -18,6 +21,17 @@
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
+
+            //var roles = new List<IdentityRole>
+            //{
+            //    new IdentityRole {Name=RoleEnum.ADMIN.ToString()},
+            //    new IdentityRole {Name=RoleEnum.HOD.ToString()},
+            //    new IdentityRole {Name=RoleEnum.INSTRUCTOR.ToString()},
+            //    new IdentityRole {Name=RoleEnum.TECHNICAL_STAFF.ToString()},
+            //    new IdentityRole {Name=RoleEnum.STUDENT.ToString()},
+            //};
+            //roles.ForEach(s => context.Roles.Add(s));
+            //context.SaveChanges();
         }
     }
 }
---
+++ LabSem3/Models/Complaint.cs
@@ -0,0 +1,13 @@
+﻿using LabSem3.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LabSem3.Models
+{
+    public class Complaint
+    {
+        
+    }
+}
