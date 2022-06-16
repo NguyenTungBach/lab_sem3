ac022184280503ec672dc95c8df11b1766496a34
NguyenTungBach <bachntth2010055@fpt.edu.vn>
2022-06-16 18:18:20 +0700

    bach

---
+++ LabSem3/Data/LabSem3Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LabSem3.Data
+{
+    public class LabSem3Context: IdentityDbContext<IdentityUser>
+    {
+        public LabSem3Context() : base("name=LabSem3DB")
+        {
+        }
+    }
+}
