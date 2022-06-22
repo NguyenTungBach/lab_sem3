8467ce68c946bed2fd1a1c216a647d4a77d2dd41
NguyenTungBach <bachntth2010055@fpt.edu.vn>
2022-06-22 23:05:47 +0700

    bach

---
+++ LabSem3/Models/ViewModel/AccountEditViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LabSem3.Models.ViewModel
+{
+    public class AccountEditViewModel
+    {
+        [DisplayName("Id")]
+        [Required(ErrorMessage = "Id Required")]
+        public string Id { get; set; }
+
+        [DisplayName("UserName")]
+        [Required(ErrorMessage = "UserName Required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Role Required")]
+        public string Role { get; set; }
+
+        public AccountEditViewModel(Account account)
+        {
+            Id = account.Id;
+            UserName = account.UserName;
+        }
+    }
+}
