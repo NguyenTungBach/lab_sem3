9720f27f80cf84be3233511c33711186280c603a
Nguyen Trung HIeu <65000911+hieuntth2002015@users.noreply.github.com>
2022-06-23 21:45:27 +0700

    hiu

---
+++ LabSem3/Controllers/EquipmentController.cs
@@ -0,0 +1,142 @@
+﻿using LabSem3.Data;
+using LabSem3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabSem3.Controllers
+{
+    public class EquipmentController : Controller
+    {
+        private LabSem3Context db;
+        public EquipmentController()
+        {
+            db = new LabSem3Context();
+        }
+
+        // GET: Equipment
+        public ActionResult Index()
+        {
+            var listEquipment = db.Equipments.ToList();
+            return View(listEquipment);
+        }
+
+        // GET: Equipment/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipment equipment = db.Equipments.Find(id);
+            if (equipment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipment);
+        }
+
+        // GET: Equipment/Create
+        public ActionResult Create()
+        {
+            ViewBag.Lab = db.Labs.ToList();
+            ViewBag.TypeEquipment = db.TypeEquipments.ToList();
+            return View();
+        }
+
+        // POST: Equipment/Create
+        [HttpPost]
+        public ActionResult Create(Equipment equipment)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                db.Equipments.Add(equipment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Equipment/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipment equipment = db.Equipments.Find(id);
+            if (equipment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Lab = db.Labs.ToList();
+            ViewBag.TypeEquipment = db.TypeEquipments.ToList();
+            return View(equipment);
+
+        }
+
+        // POST: Equipment/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Equipment equipment)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                var Eupdate = db.Equipments.Find(equipment.Id);
+                Eupdate.Name = equipment.Name;
+                Eupdate.Status = equipment.Status;
+                Eupdate.LabId = equipment.LabId;
+                Eupdate.TypeEquipmentId = equipment.TypeEquipmentId;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Equipment/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipment equipment = db.Equipments.Find(id);
+            if (equipment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipment);
+        }
+
+        // POST: Equipment/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                Equipment equipment = db.Equipments.Find(id);
+                db.Equipments.Remove(equipment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- LabSem3/Models/Document.cs
+++ LabSem3/Models/Document.cs
@@ -11,13 +11,20 @@ namespace LabSem3.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title Document Require")]
         public string Title { get; set; }
         [DataType(DataType.Text)]
-        public string Detail { get; set; }
 
+        [Required(ErrorMessage = "Detail Document Require")]
+        public string Detail { get; set; }
+       
         public int Status { get; set; }
+
         public DateTime? CreatedAt { get; set; }
+
         public DateTime? UpdatedAt { get; set; }
+
         public DateTime? DeletedAt { get; set; }
 
         public int EquipmentId { get; set; }
