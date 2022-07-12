bdb4b46ac45cfc7e636af256fd23825d86d49362
NguyenTungBach <bachntth2010055@fpt.edu.vn>
2022-07-12 19:44:56 +0700

    bach fix template

--- LabSem3/Models/ViewModel/ScheduleViewModel/ScheduleEditViewModel.cs
+++ LabSem3/Models/ViewModel/ScheduleViewModel/ScheduleEditViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -8,16 +9,19 @@ namespace LabSem3.Models.ViewModel.ScheduleViewModel
 {
     public class ScheduleEditViewModel
     {
+        [DisplayName("Date Booking")]
         [DataType(DataType.Date)]
         [Required(ErrorMessage = "DateBoking Required")]
         public DateTime DateBoking { get; set; }
-        
+
+        [DisplayName("Slot Number")]
         [Required(ErrorMessage = "SlotNumber Required")]
         public int SlotNumber { get; set; }
 
+        [DisplayName("Lab Id")]
         public int LabId { get; set; }
         public int Status { get; set; }
-
+        [DisplayName("Instructor Id")]
         public string InstructorId { get; set; }
         public ScheduleEditViewModel()
         {
